93a8e86598128282b08895de3e74fea203dff967
g4gops <gopi.rajendran@gmail.com>
2015-02-15 14:50:37 +0000

    First check in for the solution

---
+++ Nasa/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Nasa
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtResults = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(94, 200);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 30);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Ok";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(27, 41);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(229, 20);
+            this.txtInput.TabIndex = 1;
+            this.txtInput.Validating += new System.ComponentModel.CancelEventHandler(this.txtInput_Validating);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Please enter the Input.";
+            // 
+            // txtResults
+            // 
+            this.txtResults.Location = new System.Drawing.Point(27, 98);
+            this.txtResults.Multiline = true;
+            this.txtResults.Name = "txtResults";
+            this.txtResults.Size = new System.Drawing.Size(229, 82);
+            this.txtResults.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Results";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtResults);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtResults;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Nasa/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Nasa
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtResults.Clear();
+            try
+            {
+                string input = string.Empty;
+                input = txtInput.Text;
+
+                string[] splitStrs = input.Split(' ');
+                int gridSize = Int32.Parse(splitStrs[1]);
+                var arr = new int[gridSize, gridSize];
+
+
+                string stemp = String.Join(",", splitStrs, 2, splitStrs.Length - 2);
+                string[] splitStrsActual = stemp.Split(',');
+                int index = 0;
+
+                Dictionary<string, int> dictResults = new Dictionary<string, int>();
+
+                // convert 1d - 2d
+                for (int row = 0; row < gridSize; row++)
+                {
+                    for (int column = 0; column < gridSize; column++)
+                    {
+                        arr[row, column] = int.Parse(splitStrsActual[index++]);
+                        Console.WriteLine(string.Format("{0},{1} = {2}", row, column, arr[row, column]));
+                    }
+                }
+
+
+                // get the Scores and co-Ordinates
+                for (int row = 0; row < gridSize; row++)
+                {
+                    for (int column = 0; column < gridSize; column++)
+                    {
+                        var neighbMeasures = GetNeighbourMeasurements(arr, row, column);
+                        int total = neighbMeasures.Sum();
+                        dictResults.Add("(" + column.ToString() + ',' + row.ToString() + " Score:" + total.ToString() + ')', total);
+                    }
+                }
+
+
+                // Sort and Display only the top few.
+                int resultsCount = Int32.Parse(splitStrs[0]);
+                int dictIndex = 0;
+
+                foreach (KeyValuePair<string, int> item in dictResults.OrderByDescending(key => key.Value))
+                {
+                    Console.WriteLine("Key: {0}, Value: {1}", item.Key, item.Value);
+                    txtResults.AppendText(item.Key);
+                    ++dictIndex;
+                    if (dictIndex == resultsCount)
+                    {
+                        break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message); ;
+            }
+
+
+        }
+
+        /// <summary>
+        /// Method to get the nearest numbers in an 2D array
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="arr"></param>
+        /// <param name="row"></param>
+        /// <param name="column"></param>
+        /// <returns></returns>
+        public IEnumerable<T> GetNeighbourMeasurements<T>(T[,] arr, int row, int column)
+        {
+            int rows = arr.GetLength(0); // number of rows
+            int columns = arr.GetLength(1); // number of columns
+
+
+            // for each row, for cach column, return the current array of Row and Column
+
+            for (int rCtr = row - 1; rCtr <= row + 1; rCtr++)
+                for (int cCtr = column - 1; cCtr <= column + 1; cCtr++)
+                    if (cCtr >= 0 && rCtr >= 0 && cCtr < columns && rCtr < rows)
+                        yield return arr[rCtr, cCtr];
+        }
+
+        /// <summary>
+        /// Basic Validation for the Input
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void txtInput_Validating(object sender, CancelEventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtInput.Text.Trim()))
+            {
+                MessageBox.Show("Please enter a valid input");
+            }
+        }
+
+        /// <summary>
+        /// Sets the Text box to be focussed
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.ActiveControl = txtInput;
+            txtInput.Focus();
+        }
+    }
+}
